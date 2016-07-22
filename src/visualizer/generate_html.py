94a0fcf3c706713aceaf8337ca4237f2db7bb0e3
Alexander Rush <srush@sum1gpu02.rc.fas.harvard.edu>
2016-07-22 13:58:37 -0400

    vis

--- src/visualizer/generate_html.py
+++ src/visualizer/generate_html.py
@@ -1,6 +1,6 @@
 # adapted from https://github.com/steerapi/seq2seq-show-att-tell/blob/master/visualizer/generate_html.py
 
-import os, sys, copy, argparse, shutil
+import os, sys, copy, argparse, shutil, pickle
 import numpy as np
 
 def main(arguments):
@@ -10,6 +10,8 @@ def main(arguments):
     assert os.path.exists(html_head_path), 'HTML template %s not found'%html_head_path
     html_tail_path = os.path.join(script_dir, 'visualizer.html.template.tail')
     assert os.path.exists(html_tail_path), 'HTML template %s not found'%html_tail_path
+    freq_path = os.path.join(script_dir, 'freq.pkl')
+    assert os.path.exists(freq_path), 'Freq dict %s not found'%freq_path
 
     parser = argparse.ArgumentParser()
     parser.add_argument('--output_dir', dest='output_dir', default='results', help=("Output directory containing results.txt"),
@@ -35,6 +37,7 @@ def main(arguments):
     img_dir = os.path.join(website_dir, 'images')
     if not os.path.exists(img_dir):
         os.makedirs(img_dir)
+    words = pickle.load(open(freq_path))
     with open(result_path) as fin:
         with open(html_head_path) as fhead:
             with open(html_tail_path) as ftail:
@@ -44,27 +47,24 @@ def main(arguments):
                     fout.write('\n')
                     for line in fin:
                         items = line.strip().split('\t')
-                        if len(items) == 3:
-                            img_path, label_gold, label_pred = items
-                            img_base_name = img_path.replace('/', '_')
+                        if len(items) == 5:
+                            img_path, label_gold, label_pred, score_pred, score_gold = items
+                            freq_gold = words[label_gold]
+                            img_base_name = img_path.replace('/', '_')[2:]
                             img_path = os.path.join(data_base_dir, img_path)
                             img_new_path = os.path.join(img_dir, img_base_name)
                             shutil.copy(img_path, img_new_path)
                             img_rel_path = os.path.relpath(img_new_path, website_dir)
                             if label_gold == label_pred:
                                 s = '<li class="f-correct f-all">\n'
-                                s += '<img src=%s /><br/>\n'%(img_rel_path)
-                                s += 'gold: %s<br/>\n'%label_gold
-                                s += 'predicted: %s<br/>\n'%label_pred
-                                s += '</li>\n'
-                                s += '\n'
                             else:
                                 s = '<li class="f-incorrect f-all">\n'
-                                s += '<img src=%s /><br/>\n'%(img_rel_path)
-                                s += 'gold: %s<br/>\n'%label_gold
-                                s += 'predicted: %s<br/>\n'%label_pred
-                                s += '</li>\n'
-                                s += '\n'
+                            s += '<img src=%s /><br/>\n'%(img_rel_path)
+                            s += 'gold: %s (%s)<br/>\n'%(label_gold, score_gold)
+                            s += 'predicted: %s (%s)<br/>\n'%(label_pred, score_pred)
+                            s += 'gold frequency: %d out of 7.2M<br/>\n'%(freq_gold)
+                            s += '</li>\n'
+                            s += '\n'
                         fout.write(s)
 
 
